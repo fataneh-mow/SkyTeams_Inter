1d63fcf2b1e5a563b8b963c0cf9560093b01d646
fataneh-mow <fataneh.mow@gmail.com>
2025-11-14 22:41:51 +0430

    question2.js file is built

---
+++ week1-fixed/src/question2.js
@@ -0,0 +1,32 @@
+/**
+ * Author: Fatana Mawlawizadeh
+ 
+* Date: Nov, 13rd, 2025
+
+* Question: Write a function to check for even, odd or prime numbers
+
+*/
+function isEven(n) {
+    if (n % 2 === 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+}
+
+function isPrime (number) {
+    if(number < 2){
+        console.log(`${number} must be greater than 2 to check if it's prime or not!`);
+        return; 
+    }
+    
+    for(let i = 2; i < Math.ceil(number / 2); i++){
+        if(number % i === 0){
+            console.log(`${number} is not prime`);
+            return false;
+        }
+    }
+    
+    console.log(`${number} is prime`);
+    return true;
+}
