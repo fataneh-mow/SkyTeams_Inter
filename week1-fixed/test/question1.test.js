7b21b2fefb5628fb813994694bb160df61852b04
fataneh-mow <fataneh.mow@gmail.com>
2025-11-14 22:24:27 +0430

    question1.test.js modified again

--- week1-fixed/test/question1.test.js
+++ week1-fixed/test/question1.test.js
@@ -1,12 +1,12 @@
-import { it, direction, expect } from "vitest";
+import { it, describe, expect } from "vitest";
 import { swap } from "../src/question1";
 
 describe("swap function", () => {
-     it("swap two values", () => {
-        expect(swap(2, 3)).toBe(3, 2);
-    });
+  it("swaps two values", () => {
+    expect(swap(2, 3)).toEqual([3, 2]);
+  });
 
-    it("works with zero", () => {
-        expect(swap(0, 10)).toBe(10, 0);
-    }); 
+  it("works with zero", () => {
+    expect(swap(0, 10)).toEqual([10, 0]);
+  });
 });
