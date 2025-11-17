92bdf3a95702d19212e36292d480df681b799203
fataneh-mow <fataneh.mow@gmail.com>
2025-11-17 14:53:57 +0430

    question2 modified

--- week2/src/question2.js
+++ week2/src/question2.js
@@ -1,13 +1,30 @@
 /**
- * Author: Fatana Mawlawizadeh
- 
- * Date: Nov, 16th, 2025
+    * Author: Fatana Mawlawizadeh
+     
+    * Date: Nov, 16th, 2025
 
- * Question: Write a function that finds greatest and largest of 3 numbers
+    * Question: Write a function that finds greatest and largest of 3 numbers
 
- */
+*/
 
 function minMax (a, b, c) {
-    a, b, c = items ;
-    console.log(items);
+
+    items = [a, b, c];
+
+    let max = 0;
+    for (let i = 0; i < items.length; i++) {
+        if (items[i] > max) {
+            max = items[i];
+        }
+    }
+    console.log(maxx);
+
+    let min = items[0];
+    for ( i = 0; i < items.length; i++) {
+        if (min > items[i]){
+            min = items[i];
+        }
+    }
+    console.log(min);
 }
+minMax(1,2,3);
