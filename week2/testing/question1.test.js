469bc8d09b1466b25954f4d90a5592f22e016ede
fataneh-mow <fataneh.mow@gmail.com>
2025-11-18 21:04:57 +0430

    question1.test.js modified

--- week2/testing/question1.test.js
+++ week2/testing/question1.test.js
@@ -12,3 +12,23 @@
     a = 10, b = 5
 
  */
+import { swapVariables } from "../src/question1";
+import { it, describe, expect } from "vitest";
+
+describe("swapVariables function swaps two variables", () => {
+    it("swapping two variabels", () => {
+        expect(swapVariables("Hello", "world")).toEqual(["world", "Hello"]);
+        expect(swapVariables("Apple", 10)).toEqual([10, "Apple"]);
+        expect(swapVariables(-10, 10)).toEqual([10, -10]);
+        expect(swapVariables("a", "a")).toEqual(["a", "a"]);
+    });
+    it("swapping two arrays", () => {
+        expect(swapVariables([1, 2, 3, 4, 5], [])).toEqual([[], [1, 2, 3, 4, 5]]);
+    });
+    it("swapping two objects", () => {
+        expect(swapVariables({x:1, y:2}, {z:3})).toEqual([{z:3}, {x:1, y:2}]);
+    });
+    it("swapping undefined and null", () => {
+        expect(swapVariables(null, undefined)).toEqual([undefined, null]);
+    });
+});
