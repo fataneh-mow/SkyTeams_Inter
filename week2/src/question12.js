4374d0007bb69adc0b5e8b679a4596e816b8eac7
fataneh-mow <fataneh.mow@gmail.com>
2025-11-19 14:34:45 +0430

    question12.js initialized and modified

---
+++ week2/src/question12.js
@@ -0,0 +1,34 @@
+/**
+    * Author: Fatana Mawlawizadeh
+     
+    * Date: Nov, 19th, 2025
+
+    * Question 11:  Simple Calculator using switch-case 
+    Description: Input two numbers and an operator (+, -, *, /) and compute the result. 
+    Example: 
+    Input: 
+    5, 3, + 
+    Output: 
+    8     
+   
+*/
+
+export function trafficLight (color) {
+    if (typeof color !== "string") {
+        return `${color} is not applicable`;
+    };
+
+    switch (color.toLocaleLowerCase().trim()) {
+        case "red":
+            return "Stop"; 
+        case "yellow":
+            return "Ready";  
+        case "green":
+            return "Go";
+        default:
+            return `${color} is not applicable`
+    };
+};
+
+// let test = trafficLight("  rED   ");
+// console.log(test);
